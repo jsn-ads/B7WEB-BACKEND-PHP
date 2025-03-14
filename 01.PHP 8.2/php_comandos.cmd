ed625ce89800e2c00e41c1dccb2a45200a73e9af
Neto Home <jsn.ads@gmail.com>
2025-03-13 23:01:23 -0300

    PHP 8,2 - Configurações

---
+++ 01.PHP 8.2/php_comandos.cmd
@@ -0,0 +1,24 @@
+==================================================EXTENSÕES VSCODE==============================================
+
+== 1.Temas :
+
+    Monokai Charcoal high contrast
+
+== 2.Abrir projetos no Navegador:
+
+    Open PHP/HTML/JS In Browser
+
+== 3.Auto Complete: 
+    
+    @REM Desabilitar PHP language Features seach "@builtin php"
+    PHP intelephense 
+
+    PHP Namespace Resolver
+
+== 4.Icons :
+
+    vscode-icons
+
+== 5.GIT
+
+    Git History
