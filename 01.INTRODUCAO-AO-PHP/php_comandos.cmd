1f1ab79e6e382c8adab1c4167678b38221d30c1b
Neto Home <jsn.ads@gmail.com>
2025-08-04 22:29:19 -0300

    BACKEND-PHP: INTRODUÇÃO - Conceito

---
+++ 01.INTRODUCAO-AO-PHP/php_comandos.cmd
@@ -0,0 +1,43 @@
+==================================================EXTENSÕES VSCODE==============================================
+
+== 1.Temas :
+
+    Monokai Charcoal high contrast
+
+== 2.Abrir projetos no Navegador:
+
+    Open PHP/HTML/JS In Browser
+
+== 3.Auto Complete: 
+    
+    @REM Desabilitar PHP language Features seach "@builtin php"
+    PHP intelephense 
+
+    PHP Namespace Resolver
+
+== 4.Icons :
+
+    vscode-icons
+
+== 5.GIT
+
+    Git History
+
+== 6.CONFIGURAÇÕES 
+
+    Baixar fira code e instalar.
+
+    adicionar no VSCODE settings.json :
+        "editor.fontFamily": "Fira Code",
+        "editor.fontLigatures": true,
+        "editor.fontSize": 14,
+        "editor.letterSpacing": 0.5,
+        "editor.lineHeight": 24,
+        "editor.autoIndent": "advanced",
+        "editor.snippetSuggestions": "bottom",
+
+        "intelephense.format.enable": true,
+        "intelephense.completion.triggerParameterHints": true,
+        "intelephense.completion.fullyQualifyGlobalConstantsAndFunctions": true,
+        "intelephense.completion.insertUseDeclaration": true,
+        "intelephense.trace.server": "messages"
